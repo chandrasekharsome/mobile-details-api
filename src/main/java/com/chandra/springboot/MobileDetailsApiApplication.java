7115ca70dac46d50238bab3696f7e14c70df8dab
{name} <softech.series@gmail.com>
2022-09-13 15:09:07 +0530

    initial commit

---
+++ src/main/java/com/chandra/springboot/MobileDetailsApiApplication.java
@@ -0,0 +1,13 @@
+package com.chandra.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileDetailsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileDetailsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chandra/springboot/MobileDetailsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chandra.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileDetailsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
